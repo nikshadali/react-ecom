a28097b31931c5654892330d648f864506591b2a
nikshad <nikali92100@gmail.com>
2023-01-05 15:14:20 +0500

    create Categories component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Slider from "../../components/Slider/Slider"
 import FeatureProduct from "../../components/FeatureProduct/FeatureProduct"
-
+import Categories from "../../components/Categories/Categories"
 const Home = () => {
   return (
     <div className="home">
       <Slider />
       <FeatureProduct type='featured'/>
+      <Categories />
       <FeatureProduct type='trending'/>
     </div>
   )
